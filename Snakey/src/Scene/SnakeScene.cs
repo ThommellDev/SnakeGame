c184894756696b9331452101c2f198dbb6c8af38
Thommell <thomslaghekke@outlook.com>
2025-08-28 23:24:06 +0200

    [SNAKE-16] Created blueprints for the Snake scene and SceneHandler #doing

---
+++ Snakey/src/Scene/SnakeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Snakey.GameObjects;
+
+namespace Snakey;
+
+public class SnakeScene {
+    public List<GameObject> objectsInScene;
+
+    public void Initialize() {
+        
+    }
+
+    public void Load() {
+        
+    }
+    public void Update(GameTime pGameTime) {}
+    public void Draw(SpriteBatch pSpriteBatch) {}
+
+    public void AddObject(GameObject pGameObject) {
+    }
+}
