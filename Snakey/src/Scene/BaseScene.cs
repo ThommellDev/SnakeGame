b279b5d8743d37b81bc0822236ce6cdc6b2e5886
Thommell <thomslaghekke@outlook.com>
2025-08-30 14:59:09 +0200

    WIP from develop

---
+++ Snakey/src/Scene/BaseScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Snakey.GameObjects;
+
+namespace Snakey;
+
+public class BaseScene {
+    private List<GameObject> objectsInScene = new();
+    public virtual void Initialize() {}
+    public virtual void Load() {}
+
+    public virtual void Update(GameTime pGameTime) {
+        foreach (GameObject obj in objectsInScene) {
+            if (!obj.IsActive) continue;
+            obj.UpdateObject(pGameTime);
+        }
+    }
+
+    public virtual void Render(SpriteBatch pSpriteBatch) {
+        foreach (GameObject obj in objectsInScene) {
+            if (!obj.IsActive) continue;
+            obj.RenderObject(pSpriteBatch);
+        }
+    }
+}
--- Snakey/src/Scene/SnakeScene.cs
+++ Snakey/src/Scene/SnakeScene.cs
@@ -1,23 +1,38 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Snakey.GameObjects;
 
 namespace Snakey;
 
-public class SnakeScene {
-    public List<GameObject> objectsInScene;
-
+public class SnakeScene : BaseScene {
     public void Initialize() {
         
     }
 
     public void Load() {
-        
+        GameObject textureTest = new GameObject("");
     }
-    public void Update(GameTime pGameTime) {}
-    public void Draw(SpriteBatch pSpriteBatch) {}
+    
+    /// <summary>
+    /// Updates every GameObject in the scene.
+    /// </summary>
+    public void Update(GameTime pGameTime) {
+    }
 
-    public void AddObject(GameObject pGameObject) {
+    /// <summary>
+    /// Renders every GameObject in the scene.
+    /// </summary>
+    public void Render(SpriteBatch pSpriteBatch) {
+    }
+
+    /// <summary>
+    /// Tries adding a new object in the Scene's GameObject list.
+    /// </summary>
+    /// <param name="pGameObject">The new object.</param>
+    public void TryAddObject(GameObject pGameObject) {
+        if (objectsInScene.Any(x => ReferenceEquals(x, pGameObject))) return;
+        objectsInScene.Add(pGameObject);
     }
 }
